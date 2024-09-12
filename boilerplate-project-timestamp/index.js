629526b00032ec446d7cb63f55b7ba36bbdc1762
liana5555 <90687412+liana5555@users.noreply.github.com>
2024-09-12 22:29:05 +0200

    Update index.js

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -2,48 +2,83 @@
 // where your node app starts
 
 // init project
-var express = require("express");
+var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+
+// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+  res.json({greeting: 'hello API'});
 });
+
 
 app.get("/api/:date?", function (req, res) {
   const date_string = req.params.date;
+
+  let response = {unix: null, utc: null};
+  
+
   if (!date_string) {
     const dateNow = Date.now();
 
     return res.json({
       unix: parseInt(dateNow),
-      utc: new Date(dateNow).toUTCString(),
+      utc: new Date(dateNow).toUTCString()
     });
   }
-  const regex = /[^0-9]/g;
-  if (date_string.match(regex) !== null) {
-    return res.json({ error: "Invalid Date" });
-  }
+ 
+  let date;
+ 
 
-  const date = new Date(Number(date_string));
-  console.log(date);
-  return res.json({ unix: parseInt(date_string), utc: date.toUTCString() });
-});
+  
+  if (/^\d+$/.test(date_string)) {
+    date = new Date(Number(date_string));
+
+    response = { unix: Number(date_string),
+      utc: date.toUTCString() };
+  }
+  else if(date_string.includes("-")) {
+    date = new Date(date_string);
+   
+    response =  {
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    }
+
+  }  
+  
+
+  
+ 
+
+  
+
+    if (response.unix === null || response.utc === "Invalid Date") {
+      return res.json({error: "Invalid Date"})
+    }
+    else {
+      return res.json(response);
+    }
+
+  
+
+})
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
